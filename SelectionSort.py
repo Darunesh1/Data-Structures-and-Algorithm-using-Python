56110fc156053567b7934d45f3b7a596593c1af1
Darunesh1 <daru22018.it@rmkec.ac.in>
2024-07-04 18:13:56 +0530

    implemented selection sort

---
+++ SelectionSort.py
@@ -0,0 +1,20 @@
+def SelectionSort(l):
+    n=len(l)
+    if n<1:
+        return l
+    
+    for i in range(n):
+        m=i
+        
+        for j in range(i+1,n):
+            if l[m]>l[j]:
+                m=j
+            
+            
+        l[i],l[m]=l[m],l[i]
+    return l
+
+
+l=eval(input())
+print(SelectionSort(l))
+        
