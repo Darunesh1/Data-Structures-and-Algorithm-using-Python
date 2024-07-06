10bf8a4c435e9ced40fd5fb041f60488187437d0
Darunesh1 <daru22018.it@rmkec.ac.in>
2024-07-06 16:00:48 +0530

    seperated que and BFS

---
+++ bfs.py
@@ -0,0 +1,69 @@
+import numpy as np
+from queue_module import Queue
+
+def neighbours(AMat, v):
+    nbrs = []
+    rows, cols = AMat.shape
+    for i in range(rows):
+        if AMat[v, i] == 1:
+            nbrs.append(i)
+    return nbrs
+
+def BFS(AMat, v):
+    rows, cols = AMat.shape
+    visited = {}
+    for i in range(rows):
+        visited[i] = False 
+    q = Queue()  # Instantiate the Queue
+
+    visited[v] = True
+    q.addq(v)
+    
+    while not q.isempty():
+        j = q.deq()
+        for k in neighbours(AMat, j):
+            if not visited[k]:
+                visited[k] = True
+                q.addq(k)
+    
+    return visited
+
+def BFSList(Alist, v):
+    visited, parent = {}, {}
+    for i in Alist.keys():
+        visited[i] = False
+        parent[i] = None
+        
+    q = Queue()  # Instantiate the Queue
+    visited[v] = True
+    q.addq(v)
+    
+    while not q.isempty():
+        j = q.deq()
+        for k in Alist[j]:
+            if not visited[k]:
+                visited[k] = True
+                parent[k] = j
+                q.addq(k)
+                
+    return (visited, parent)
+
+def BFSLevel(Alist, v):
+    level, parent = {}, {}
+    for i in Alist.keys():
+        level[i] = -1
+        parent[i] = None
+        
+    q = Queue()  # Instantiate the Queue
+    q.addq(v)
+    level[v] = 0
+    
+    while not q.isempty():
+        j = q.deq()
+        for k in Alist[j]:
+            if level[k] == -1:
+                level[k] = level[j] + 1
+                parent[k] = j
+                q.addq(k)
+                
+    return (level, parent)
--- queue_module.py
+++ queue_module.py
@@ -1,68 +1,19 @@
 class Queue:
     def __init__(self):
-        self.queue=[]
+        self.queue = []
         
     def __str__(self):
         return str(self.queue)
         
-    def addq(self,v):
+    def addq(self, v):
         self.queue.append(v)
         
     def isempty(self):
-        return self.queue==[]
+        return self.queue == []
     
     def deq(self):
-        v=None
+        v = None
         if not self.isempty():
-            v=self.queue[0]
-            self.queue=self.queue[1:]
+            v = self.queue[0]
+            self.queue = self.queue[1:]
         return v
- 
-
-def neighbours(AMat,v):
-    nbrs=[]
-    rows,cols=AMat.shape
-    for i in range(rows):
-        if AMat[v,i]==1:
-            nbrs.append(i)
-    return nbrs
-   
-    
-def BFS(AMat,v):
-    rows,cols=AMat.shape
-    visited={}
-    for i in range(rows):
-        visited[i]=False 
-    q=Queue
-    
-    
-    visited[v]=True
-    q.addq(v)
-    
-    while not q.isempty():
-        j=q.deq()
-        for k in neighbours(AMat,j):
-            if not visited[k]:
-                visited[k]=True
-                q.addq(k)
-    
-    return visited
-
-
-def BFSList(Alist,v):
-    vistited,parent={},{}
-    for i in Alist.keys():
-        vistited[i]=True
-        parent[i]=None
-        
-    q=Queue
-    
-    while not q.isempty():
-        j=q.deq()
-        for k in Alist[j]:
-            if not vistited[k]:
-                vistited[k]=True
-                parent[k]=j
-    return (vistited,parent)
-    
-    
